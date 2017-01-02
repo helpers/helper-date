0550d3af307eb694d49e4afd5ea7fbdb439b895e
jonschlinkert <jon.schlinkert@sellside.com>
2017-01-02 00:49:57 -0500

    use assert

--- test.js
+++ test.js
@@ -7,55 +7,54 @@
 
 'use strict';
 
-/* deps: mocha */
-var should = require('should');
+require('mocha');
+var assert = require('assert');
 var handlebars = require('handlebars');
 var momentjs = require('moment');
-var _ = require('lodash');
-var moment = require('./');
 var date = require('date.js');
-
+var moment = require('./');
+var _ = require('lodash');
 
 describe('moment', function () {
   it('should return a default formatted moment date when nothing is passed:', function () {
-    moment().should.eql(momentjs().format('MMMM DD, YYYY'));
+    assert.deepEqual(moment(), momentjs().format('MMMM DD, YYYY'));
   });
 
   it('should return a formatted moment date:', function () {
-    moment('YYYY').should.eql(momentjs().format('YYYY'));
-    moment('MMMM DD, YYYY').should.eql(momentjs().format('MMMM DD, YYYY'));
+    assert.deepEqual(moment('YYYY'), momentjs().format('YYYY'));
+    assert.deepEqual(moment('MMMM DD, YYYY'), momentjs().format('MMMM DD, YYYY'));
   });
 
   it('should parse a human-readable date with date.js and return a formatted moment date:', function () {
-    moment('1 year ago', 'YYYY').should.eql((+momentjs().format('YYYY') - 1).toString());
-    moment('1 year from now', 'YYYY').should.eql((+momentjs().format('YYYY') + 1).toString());
-    moment('10 years ago', 'YYYY').should.eql((new Date().getFullYear() - 10).toString());
-    moment('2005-12-25', 'YYYY', {datejs: false}).should.eql('2005');
-    moment(date('1 year ago'), 'YYYY').should.eql((+moment('YYYY') - 1).toString());
-    moment(date('1 year from now'), 'YYYY').should.eql((+moment('YYYY') + 1).toString());
-    moment(date('10 years ago'), 'YYYY').should.eql((new Date().getFullYear() - 10).toString());
-    moment(date('Next year.'), 'YYYY').should.eql((+moment('YYYY') + 1).toString());
-    moment(date('This year.'), 'YYYY').should.eql(moment('YYYY'));
+    assert.deepEqual(moment('1 year ago', 'YYYY'), (+momentjs().format('YYYY') - 1).toString());
+    assert.deepEqual(moment('1 year from now', 'YYYY'), (+momentjs().format('YYYY') + 1).toString());
+    assert.deepEqual(moment('10 years ago', 'YYYY'), (new Date().getFullYear() - 10).toString());
+    assert.deepEqual(moment('2005-12-25', 'YYYY', {datejs: false}), '2005');
+    assert.deepEqual(moment(date('1 year ago'), 'YYYY'), (+moment('YYYY') - 1).toString());
+    assert.deepEqual(moment(date('1 year from now'), 'YYYY'), (+moment('YYYY') + 1).toString());
+    assert.deepEqual(moment(date('10 years ago'), 'YYYY'), (new Date().getFullYear() - 10).toString());
+    assert.deepEqual(moment(date('Next year.'), 'YYYY'), (+moment('YYYY') + 1).toString());
+    assert.deepEqual(moment(date('This year.'), 'YYYY'), moment('YYYY'));
   });
 
   it('should work as a lodash helper:', function () {
     var locals = {imports: {moment: moment, date: date}};
-    _.template('<%= moment(date("5 years ago"), "YYYY") %>', locals)().should.eql((moment("YYYY") - 5).toString());
-    _.template('<%= moment("5 years ago", "YYYY") %>', locals)().should.eql((moment("YYYY") - 5).toString());
-    _.template('<%= moment("MMMM DD, YYYY") %>', locals)().should.eql(moment("MMMM DD, YYYY"));
-    _.template('<%= moment("MMMM DD, YYYY") %>', locals)().should.eql(momentjs().format("MMMM DD, YYYY"));
-    _.template('<%= moment("YYYY") %>', locals)().should.eql(new Date().getFullYear().toString());
+    assert.deepEqual(_.template('<%= moment(date("5 years ago"), "YYYY") %>', locals)(), (moment("YYYY") - 5).toString());
+    assert.deepEqual(_.template('<%= moment("5 years ago", "YYYY") %>', locals)(), (moment("YYYY") - 5).toString());
+    assert.deepEqual(_.template('<%= moment("MMMM DD, YYYY") %>', locals)(), moment("MMMM DD, YYYY"));
+    assert.deepEqual(_.template('<%= moment("MMMM DD, YYYY") %>', locals)(), momentjs().format("MMMM DD, YYYY"));
+    assert.deepEqual(_.template('<%= moment("YYYY") %>', locals)(), new Date().getFullYear().toString());
   });
 
   it('should work as a handlebars helper:', function () {
     handlebars.registerHelper('date', moment);
     var locals = {formatDate: "MMMM DD, YYYY", time: new Date()};
 
-    handlebars.compile('{{date "5 years ago" "YYYY"}}')().should.eql((moment("YYYY") - 5).toString());
-    handlebars.compile('{{date time "MMMM DD, YYYY"}}')().should.eql(moment("MMMM DD, YYYY"));
-    handlebars.compile('{{date time "MMMM DD, YYYY"}}')({time: new Date()}).should.eql(momentjs().format("MMMM DD, YYYY"));
-    handlebars.compile('{{date time formatDate}}')(locals).should.eql(momentjs().format("MMMM DD, YYYY"));
-    handlebars.compile('{{date time formatDate}}')(locals).should.eql(moment("MMMM DD, YYYY"));
+    assert.deepEqual(handlebars.compile('{{date "5 years ago" "YYYY"}}')(), (moment("YYYY") - 5).toString());
+    assert.deepEqual(handlebars.compile('{{date time "MMMM DD, YYYY"}}')(), moment("MMMM DD, YYYY"));
+    assert.deepEqual(handlebars.compile('{{date time "MMMM DD, YYYY"}}')({time: new Date()}), momentjs().format("MMMM DD, YYYY"));
+    assert.deepEqual(handlebars.compile('{{date time formatDate}}')(locals), momentjs().format("MMMM DD, YYYY"));
+    assert.deepEqual(handlebars.compile('{{date time formatDate}}')(locals), moment("MMMM DD, YYYY"));
   });
 });
 
